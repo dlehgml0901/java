028e7a7a53935729bd8eba1d3c0cbd3f0d1dae4b
dlehgml0901 <dlehgml0901@example.com>
2020-04-08 11:54:30 +0900

    0408 11:54

---
+++ inheritance/Main.java
@@ -0,0 +1,20 @@
+package com.jse.inheritance;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.println("0.Exit, 1.Go");
+			switch (scan.nextInt()) {
+			case 0:
+				
+				return;
+			case 1:
+				break;
+			}
+		}
+	}
+}
